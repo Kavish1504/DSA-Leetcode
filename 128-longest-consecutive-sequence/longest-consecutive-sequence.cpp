38561cb57dfc8b2d1f9a7ee587c2f2f389c893ca
Kavish1504 <kgupta9_be23@thapar.edu>
2026-07-24 20:24:09 +0530

    Time: 89 ms (24.37%) | Memory: 89 MB (25.57%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int ans=0;
+        for(int x:nums){
+            if(mp.count(x)){
+                continue;
+            }
+            int left=mp.count(x-1)?mp[x-1]:0;
+            int right=mp.count(x+1)?mp[x+1]:0;
+            int len=left+right+1;
+            mp[x]=len;
+            mp[x-left]=len;
+            mp[x+right]=len;
+            ans=max(ans,len);
+        }
+        return ans;
+    }
+};
